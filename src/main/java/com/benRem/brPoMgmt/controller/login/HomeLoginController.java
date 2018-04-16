532a218956cd0b8ff39477d0e9e1977040609782
debnathchatterjee <debnathch@gmail.com>
2018-04-16 08:19:17 +0530

    social security google login with authentication

--- src/main/java/com/benRem/brPoMgmt/controller/login/HomeLoginController.java
+++ src/main/java/com/benRem/brPoMgmt/controller/login/HomeLoginController.java
@@ -1,6 +1,9 @@
 package com.benRem.brPoMgmt.controller.login;
 
+import com.benRem.brPoMgmt.reqResObj.response.AjaxResponseBody;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,4 +27,19 @@ public class HomeLoginController {
         return new RedirectView("productShop/login.html");
     }
 
+
+    @RequestMapping(value ="/adminLogin", method = RequestMethod.POST)
+    public ResponseEntity<?> adminLogin(String password)  {
+
+        AjaxResponseBody result = new AjaxResponseBody();
+
+        // add to cart to table
+        if(password.equalsIgnoreCase("ADMINBR")) {
+
+            result.setMsg("success");
+            return ResponseEntity.ok(result) ;
+        } else
+            return ResponseEntity.ok(HttpStatus.EXPECTATION_FAILED);
+
+    }
 }

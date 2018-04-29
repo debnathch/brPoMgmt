456448b675e29ec4aee2c8327fc2ebcb12bdcc97
debnathchatterjee <debnathch@gmail.com>
2018-04-29 17:25:55 +0530

    hedoku cfedentials added for clearDB

--- src/main/java/META-INF/resources/static/js/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// Creating angular Application with module name "GoogleOAuthMicroservice"
-var app = angular.module('SocialOAuth2Microservice',[]);
-
-app.config(['$httpProvider', function ($httpProvider) {
-    $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-}]);

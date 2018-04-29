e46ae8d33095dee485d334fa0fc2c9a4f3f1ba9b
debnathchatterjee <debnathch@gmail.com>
2018-04-29 16:46:36 +0530

    hedoku cfedentials added for clearDB

---
+++ src/main/java/com/benRem/brPoMgmt/services/StaticResourceConfiguration.java
@@ -0,0 +1,22 @@
+package com.benRem.brPoMgmt.services;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by debnathchatterjee on 29/04/18.
+ */
+@Configuration
+public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+}

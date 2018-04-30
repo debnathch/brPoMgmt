8277cf8ba6822b87ad810147e20e42da726c61be
debnathchatterjee <debnathch@gmail.com>
2018-05-01 00:26:46 +0530

    hedoku cfedentials added for clearDB

--- src/main/java/com/benRem/brPoMgmt/services/login/OAuthSecurityConfig.java
+++ src/main/java/com/benRem/brPoMgmt/services/login/OAuthSecurityConfig.java
@@ -66,7 +66,7 @@ public class OAuthSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http
+       /* http
                 // Starts authorizing configurations.
                 .authorizeRequests()
                 // Ignore the "/" and "/index.html"
@@ -84,7 +84,7 @@ public class OAuthSecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilterAt(filter(), BasicAuthenticationFilter.class)
                 .csrf()
                 //.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-                .disable();
+                .disable();*/
     }
 
     /*This method for creating filter for OAuth authentication.*/

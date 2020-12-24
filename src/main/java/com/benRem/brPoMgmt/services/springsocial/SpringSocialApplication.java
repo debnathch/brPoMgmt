3821971be6f598e915c29ae507e2dd01ce5a9f13
debnathchaterjee <debnath.chaterje@gmail.com>
2020-12-24 15:44:55 +0530

    Revert "change to react and working on social data fetching w.r.t recommendation"
    This reverts commit ec74cd6e

---
+++ src/main/java/com/benRem/brPoMgmt/services/springsocial/SpringSocialApplication.java
@@ -0,0 +1,14 @@
+package com.benRem.brPoMgmt.services.springsocial;
+
+
+
+/*
+@SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
+public class SpringSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocialApplication.class, args);
+	}
+}
+*/

bf19b3183cacd47e367c7a470b65ba893f5d5f87
debnathchaterjee <debnath.chaterje@gmail.com>
2021-05-27 11:10:25 +0530

    adding tag so that link of india mart can be opened in a new tab

--- src/main/java/com/benRem/brPoMgmt/controller/login/LoginController.java
+++ src/main/java/com/benRem/brPoMgmt/controller/login/LoginController.java
@@ -1,7 +1,17 @@
 package com.benRem.brPoMgmt.controller.login;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by debnathchatterjee on 10/06/17.
  */
-public interface LoginController {
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+    @RequestMapping("/")
+    public String index()  {
+        return "forward:start.html";
+    }
 }
